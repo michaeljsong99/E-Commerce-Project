3ad1fbe34f5465e5046d60706cdb7d08dbe34466
Michael Song <mjysong@gmail.com>
2020-04-06 20:37:47 -0400

    broken homepage into smaller components and styled menu items

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.component';
+
+const Homepage = () => (
+    <div classname = 'homepage'>
+        <Directory/>
+    </div>
+);
+
+export default Homepage
